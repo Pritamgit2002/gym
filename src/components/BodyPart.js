8a816c352bf333869943dc64010ac4ba310244d0
Pritam Gain <pritamjob2021@gmail.com>
2023-09-10 14:26:52 +0530

    commit

---
+++ src/components/BodyPart.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Stack,Typography } from '@mui/material'; 
+import Icon1 from '../assets/icons/gym.png';
+import Icon2 from '../assets/icons/body-part.png'
+
+const BodyPart = ({item , setBodyPart , bodyPart}) => {
+  return (
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className='bodyPart-card'
+      sx={
+        bodyPart === item ? {
+          borderTop: '4px solid #ff2625',
+          backgroundColor: '#fff',
+          borderBottomLeftRadius: '20px',
+          width: '270px',
+          height: '280px',
+          cursor: 'pointer',
+          gap: '40px'
+        } : {
+          background: '#fff',
+          borderBottomLeftRadius:'20px',
+          width: '270px',
+          height: '280px',
+          cursor: 'pointer',
+          gap: '40px'
+        }}
+        onClick={() => {
+          setBodyPart(item);
+          window.scrollTo({top: 1800 , left : 100 , behavior: 'smooth'});
+        }}
+    >
+      <img src={Icon1} alt="dumbbell" style={{width: '40px', height: "40px" }} />
+      
+      <Typography fontSize="24px" fontWeight="bold" color="#3A1212">{item}</Typography>
+
+    </Stack>
+  )
+}
+
+export default BodyPart
